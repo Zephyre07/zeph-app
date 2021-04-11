f548e4faf962c4422749b681081d62e1e1bdaef6
Asif <muhammad.asif@capgemini.com>
2021-04-11 23:59:14 +0530

    add controller

--- src/main/java/com/demo/apps/Application.java
+++ src/main/java/com/demo/apps/Application.java
@@ -1,10 +1,15 @@
 package com.demo.apps;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		 SpringApplication.run(Application.class);
 	}
 
 }
